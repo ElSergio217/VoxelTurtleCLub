3801dffad97c574d1ffd44c8f8a47f23c19a254f
Sergio <sergio.garcia@nextgencode.io>
2022-01-01 18:12:32 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 
   let pageRef = [useRef(null), useRef(null), useRef(null), useRef(null), useRef(null)];
 
-  const scrollToRef = ref => ref.current.scrollIntoView({behavior: "smooth", block: "center"});
+  const scrollToRef = ref => ref.current.scrollIntoView({behavior: "smooth", block: "start"});
   const scrollToPane = num => scrollToRef(pageRef[num]);
 
   return (
@@ -20,20 +20,17 @@ function App() {
 
       <nav class=" turtleHeader flex fixed w-screen invisible md:visible">
         <div class="flex-1 flex justify-center mr-auto self-center">
-            <a href="#responsive-header" class="text-lg mr-6 text-white font-semibold	" onClick={() =>scrollToPane(0)}>
+            <a href="#responsive-header" class="text-lg mr-6 text-white font-semibold	" onClick={() =>scrollToPane(1)}>
               ABOUT
             </a>
-            <a href="#responsive-header" class="text-lg mr-6 text-white font-semibold	" onClick={() =>scrollToPane(1)}>
+            <a href="#responsive-header" class="text-lg mr-6 text-white font-semibold	" onClick={() =>scrollToPane(2)}>
               ROADMAP
             </a>
-            <a href="#responsive-header" class="text-lg mr-6 text-white font-semibold	" onClick={() =>scrollToPane(2)}>
+            <a href="#responsive-header" class="text-lg mr-6 text-white font-semibold	" onClick={() =>scrollToPane(3)}>
               FAQ
             </a>
-            <a href="#responsive-header" class="text-lg mr-6  text-white font-semibold	" onClick={() =>scrollToPane(3)}>
-              CREATOR
-            </a>
         </div>
-        <img src={header} className='max-h-32'/>
+        <img src={header} className='max-h-32 cursor-pointer' onClick={() =>scrollToPane(0)}/>
         <div class="flex-1 flex justify-center ml-auto self-center">
           <a href="#" class="turtleButton inline-block text-lg font-semibold px-6 py-4 text-black">Coming Soon</a>
         </div>
@@ -41,7 +38,7 @@ function App() {
 
 
 
-      <div className='flex h-screen turtleHeader'>
+      <div className='flex h-screen turtleHeader'  ref={pageRef[0]}>
         <div class="grid lg:grid-cols-2 md:grid-cols-1  m-auto container">
           <img src={logo} className="w-3/4 lg:w-full "/>
           <div className='text-center m-auto w-3/4'>
@@ -57,10 +54,10 @@ function App() {
       </div>   
 
 
-      <header class="flex items-center justify-center py-80 bg-fixed bg-center bg-cover custom-img "></header>
+      <header class="flex items-center justify-center py-80 bg-fixed bg-center bg-cover custom-img " ></header>
 
 
-      <div className='flex p-6 turtleHeader' ref={pageRef[0]}>
+      <div className='flex p-6 turtleHeader' ref={pageRef[1]}>
         <div class=" m-auto container"> 
             <div class="grid lg:grid-cols-2 grid-cols-1 m-auto container text-center mt-20 ">
               <p className='text-xl lg:text-2xl text-white font-semibold whitespace-pre-wrap text-left w-full mx-auto mt-5'>
@@ -74,34 +71,38 @@ function App() {
       </div> 
 
   
-      <div className='flex py-28 turtleHeader' ref={pageRef[1]}>
+      <div className='flex py-28 turtleHeader' ref={pageRef[2]}>
         <div class=" m-auto container">
           <h1 className='text-4xl lg:text-6xl	text-white font-semibold mt-5 text-center w-full m-auto '>Voxel Turtle Roadmap</h1>   
 
           <div class="grid lg:grid-cols-4 grid-cols-1 m-auto container text-center mt-20 lg:divide-x">
 
-            <div className='p-5'>
+            <div className='p-2'>
+              <p className='text-md lg:text-md text-white font-semibold mb-2'>Phase 1</p>
               <p className='text-3xl lg:text-4xl text-white font-semibold mb-5 self-end'>20 piece Collection Rare drop</p>
               <p className='text-md lg:text-lg text-white font-semibold whitespace-pre-wrap'>
                 We will begin minting 20 1/1 rare Voxel Turtles. These will be only available for grabs on OpenSea. Holders will not only hold a rare Voxel Turtle but will also get a chance to win more exclusive Voxel Turtles in the future!
               </p>
             </div>
             
-            <div className='p-5'>
+            <div className='p-2'>
+              <p className='text-md lg:text-md text-white font-semibold mb-2'>Phase 2</p>
               <p className='text-3xl lg:text-4xl text-white font-semibold mb-5 self-end'>1,800 Voxel Turtles for mint</p>
               <p className='text-md lg:text-lg text-white font-semibold whitespace-pre-wrap'>
                 We want the rest of the world to be a part of the Voxel Turtle club too! We look forward to dropping 1,800 Voxel Turtles in 2022.
               </p>
             </div>
 
-            <div className='p-5'>
+            <div className='p-2'>
+              <p className='text-md lg:text-md text-white font-semibold mb-2'>Phase 3</p>
               <p className='text-3xl lg:text-4xl text-white font-semibold mb-5 self-end'>Augmented Reality <br/> Application </p>
               <p className='text-md lg:text-lg text-white font-semibold whitespace-pre-wrap'> We look forward to bringing your Voxel Turtle to life! We plan on developing a Augmented Reality application for Android and IOS that allows you to interract with your Voxel Turtle in the real world. </p>
 
 
             </div>
 
-            <div className='p-5'>
+            <div className='p-2'>
+              <p className='text-md lg:text-md text-white font-semibold mb-2'>Phase 4</p>
               <p className='text-3xl lg:text-4xl text-white font-semibold mb-5 self-end'>Giving Back to <br/>IRL Turtles</p>
               <p className='text-md lg:text-lg text-white font-semibold whitespace-pre-wrap'>We plan to give back for a bigger cause. Voxel Turtle Club will donate a percentage of each NFT minted to a selected organization that mission ties with rescuing sea turtles and other sea life.</p>            
             </div>
@@ -112,7 +113,7 @@ function App() {
       </div> 
 
      
-        <div class="lg:w-1/2 w-full mx-auto px-5 py-28 mx-auto " ref={pageRef[2]}>
+        <div class="lg:w-1/2 w-full mx-auto px-5 py-28 mx-auto " ref={pageRef[3]}>
           
           <div class="text-center mb-20">
             <h1 className='text-4xl lg:text-6xl	text-black font-semibold mt-5 text-center w-full m-auto '>FAQ</h1>  
@@ -122,7 +123,7 @@ function App() {
             <div class="grid grid-cols-1 lg:grid-cols-2 w-full px-4">
               <div>
                 <details class="mb-4">
-                  <summary class="py-2 px-4 text-2xl font-semibold ">
+                  <summary class="py-2 px-4 text-2xl font-semibold cursor-pointer	">
                     What are NFTs?
                   </summary>
                   <p className='px-10 text-lg font-semibold whitespace-pre-wrap'>
@@ -137,7 +138,7 @@ function App() {
                 </details>
 
                 <details class="mb-4">
-                  <summary class="py-2 px-4 text-2xl font-semibold ">
+                  <summary class="py-2 px-4 text-2xl font-semibold cursor-pointer	 ">
                     How do I get a Voxel Turtle?
                   </summary>
                   <p className='px-10 text-lg font-semibold whitespace-pre-wrap'>
@@ -149,7 +150,7 @@ function App() {
                 </details>
 
                 <details class="mb-4">
-                  <summary class="py-2 px-4 text-2xl font-semibold ">
+                  <summary class="py-2 px-4 text-2xl font-semibold cursor-pointer	">
                     How much will it cost to mint a Voxel Turtle?
                   </summary>
                   <p className='px-10 text-lg font-semibold whitespace-pre-wrap'>
@@ -160,7 +161,7 @@ function App() {
                 </details>
 
                 <details class="mb-4">
-                  <summary class="py-2 px-4 text-2xl font-semibold ">
+                  <summary class="py-2 px-4 text-2xl font-semibold cursor-pointer	">
                     When will Voxel Turtles will be available?
                   </summary>
                   <p className='px-10 text-lg font-semibold whitespace-pre-wrap'>
@@ -169,12 +170,12 @@ function App() {
                 </details>
               </div>
 
-              <img src={sleep} className='w-1/2 justify-self-center self-end '/>
+              <img src={sleep} className='w-1/2 justify-self-center self-start '/>
             </div>
           </div>
         </div>
   
-        <div className='flex py-10 turtleHeader' ref={pageRef[3]}>
+        {/* <div className='flex py-10 turtleHeader' ref={pageRef[3]}>
         <div class="grid grid-cols-1 lg:grid-cols-2 m-auto lg:w-1/2 w-full mx-auto">
 
           <img src={me} className='w-3/4 justify-self-center rounded-full'/>
@@ -187,7 +188,7 @@ function App() {
             <br/>
           </div>
         </div>
-      </div>  
+      </div>   */}
 
       <div className='flex py-6 turtleHeader'>
         <div class=" grid grid-cols-1 m-auto container">
