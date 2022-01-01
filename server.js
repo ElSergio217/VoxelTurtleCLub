ad32bf92466218528de0e5431372261a1ecbb386
Sergio <sergio.garcia@nextgencode.io>
2021-12-31 20:44:47 -0500

    first version

---
+++ server.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const express = require('express');
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.json());
+app.use("/", express.static(path.join(__dirname, "build")));
+
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, "build/index.html"));
+});
+
+app.listen(PORT, async function() {
+    console.log(`Listening on Port ${PORT}`);
+});
+
