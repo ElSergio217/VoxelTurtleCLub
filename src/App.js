6c16c4a7370fb7c68aea1ac2a0c3760aafc66ef1
Sergio <sergio.garcia@nextgencode.io>
2022-01-02 00:14:15 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import logo from './logo.png';
 import me from './me.jpg';
 import sleep from './sleep.png';
 import turle1 from './turtle1.png';
+import FadeInSection from'./components/fadeInSection'
+
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
@@ -17,8 +19,7 @@ function App() {
 
   return (
     <>
-
-      <nav class=" turtleHeader flex fixed w-screen invisible md:visible">
+      <nav class=" turtleHeader flex fixed w-screen invisible md:visible z-10">
         <div class="flex-1 flex justify-center mr-auto self-center">
             <a href="#responsive-header" class="text-lg mr-6 text-white font-semibold	" onClick={() =>scrollToPane(1)}>
               ABOUT
@@ -58,57 +59,52 @@ function App() {
 
 
       <div className='flex p-6 turtleHeader' ref={pageRef[1]}>
-        <div class=" m-auto container"> 
-            <div class="grid lg:grid-cols-2 grid-cols-1 m-auto container text-center mt-20 ">
-              <p className='text-xl lg:text-2xl text-white font-semibold whitespace-pre-wrap text-left w-full mx-auto mt-5'>
-              <h1 className='text-4xl lg:text-6xl	text-white font-semibold mt-5 w-full m-auto '>About Voxel Turtle Club</h1>  
-                <br/>
-                Voxel Turtle Club a is digitally generated collection of turtles with different types of attributes such as head wear, face style, tops and more. Each Voxel Turtle are secured by the Ethereum blockchain with one owner at a time. Our mission is to use the Voxel Turtle Club to build awareness and contribute to turtles rescue and other sea creatures. 
-              </p>
-              <img src={turle1} className='w-1/3 justify-self-center self-end '/>
-            </div>
-        </div>
+          <div class=" m-auto container"> 
+            <FadeInSection>
+              <div class="grid lg:grid-cols-2 grid-cols-1 m-auto container text-center mt-20 ">
+                <p className='text-xl lg:text-2xl text-white font-semibold whitespace-pre-wrap text-left w-full mx-auto mt-5'>
+                <h1 className='text-4xl lg:text-6xl	text-white font-semibold mt-5 w-full m-auto '>About Voxel Turtle Club</h1>  
+                  <br/>
+                  Voxel Turtle Club is a digitally generated NFT collection of turtles with different types of attributes such as head wear, face style, tops and more. Each Voxel Turtle are secured by the Ethereum blockchain with one owner at a time. Our mission is to use the Voxel Turtle Club to build awareness and contribute to turtles rescue and other sea creatures. 
+                </p>
+                <img src={turle1} className='w-1/3 justify-self-center self-end '/>
+              </div>
+            </FadeInSection>
+          </div>
       </div> 
 
   
       <div className='flex py-28 turtleHeader' ref={pageRef[2]}>
         <div class=" m-auto container">
-          <h1 className='text-4xl lg:text-6xl	text-white font-semibold mt-5 text-center w-full m-auto '>Voxel Turtle Roadmap</h1>   
-
-          <div class="grid lg:grid-cols-4 grid-cols-1 m-auto container text-center mt-20 lg:divide-x">
-
-            <div className='p-2'>
-              <p className='text-md lg:text-md text-white font-semibold mb-2'>Phase 1</p>
-              <p className='text-3xl lg:text-4xl text-white font-semibold mb-5 self-end'>20 piece Collection Rare drop</p>
-              <p className='text-md lg:text-lg text-white font-semibold whitespace-pre-wrap'>
-                We will begin minting 20 1/1 rare Voxel Turtles. These will be only available for grabs on OpenSea. Holders will not only hold a rare Voxel Turtle but will also get a chance to win more exclusive Voxel Turtles in the future!
-              </p>
+          <FadeInSection>
+            <h1 className='text-4xl lg:text-6xl	text-white font-semibold mt-5 text-center w-full m-auto '>Voxel Turtle Roadmap</h1>   
+            <div class="grid lg:grid-cols-4 grid-cols-1 m-auto container text-center mt-20 lg:divide-x">
+              <div className='p-2'>
+                <p className='text-md lg:text-md text-white font-semibold mb-2'>Phase 1</p>
+                <p className='text-3xl lg:text-4xl text-white font-semibold mb-5 self-end'>20 piece Collection Rare drop</p>
+                <p className='text-md lg:text-lg text-white font-semibold whitespace-pre-wrap'>
+                  We will begin minting 20 1/1 rare Voxel Turtles. These will be only available for grabs on OpenSea. Holders will not only hold a rare Voxel Turtle but will also get a chance to win more exclusive Voxel Turtles in the future!
+                </p>
+              </div>          
+              <div className='p-2'>
+                <p className='text-md lg:text-md text-white font-semibold mb-2'>Phase 2</p>
+                <p className='text-3xl lg:text-4xl text-white font-semibold mb-5 self-end'>1,800 Voxel Turtles for mint</p>
+                <p className='text-md lg:text-lg text-white font-semibold whitespace-pre-wrap'>
+                  We want the rest of the world to be a part of the Voxel Turtle club too! We look forward to dropping 1,800 Voxel Turtles in 2022.
+                </p>
+              </div>
+              <div className='p-2'>
+                <p className='text-md lg:text-md text-white font-semibold mb-2'>Phase 3</p>
+                <p className='text-3xl lg:text-4xl text-white font-semibold mb-5 self-end'>Augmented Reality <br/> Application </p>
+                <p className='text-md lg:text-lg text-white font-semibold whitespace-pre-wrap'> We look forward to bringing your Voxel Turtle to life! We plan on developing a Augmented Reality application for Android and IOS that allows you to interract with your Voxel Turtle in the real world. </p>
+              </div>
+              <div className='p-2'>
+                <p className='text-md lg:text-md text-white font-semibold mb-2'>Phase 4</p>
+                <p className='text-3xl lg:text-4xl text-white font-semibold mb-5 self-end'>Giving Back to <br/>IRL Turtles</p>
+                <p className='text-md lg:text-lg text-white font-semibold whitespace-pre-wrap'>We plan to give back for a bigger cause. Voxel Turtle Club will donate a percentage of each NFT minted to a selected organization that mission ties with rescuing sea turtles and other sea life.</p>            
+              </div>
             </div>
-            
-            <div className='p-2'>
-              <p className='text-md lg:text-md text-white font-semibold mb-2'>Phase 2</p>
-              <p className='text-3xl lg:text-4xl text-white font-semibold mb-5 self-end'>1,800 Voxel Turtles for mint</p>
-              <p className='text-md lg:text-lg text-white font-semibold whitespace-pre-wrap'>
-                We want the rest of the world to be a part of the Voxel Turtle club too! We look forward to dropping 1,800 Voxel Turtles in 2022.
-              </p>
-            </div>
-
-            <div className='p-2'>
-              <p className='text-md lg:text-md text-white font-semibold mb-2'>Phase 3</p>
-              <p className='text-3xl lg:text-4xl text-white font-semibold mb-5 self-end'>Augmented Reality <br/> Application </p>
-              <p className='text-md lg:text-lg text-white font-semibold whitespace-pre-wrap'> We look forward to bringing your Voxel Turtle to life! We plan on developing a Augmented Reality application for Android and IOS that allows you to interract with your Voxel Turtle in the real world. </p>
-
-
-            </div>
-
-            <div className='p-2'>
-              <p className='text-md lg:text-md text-white font-semibold mb-2'>Phase 4</p>
-              <p className='text-3xl lg:text-4xl text-white font-semibold mb-5 self-end'>Giving Back to <br/>IRL Turtles</p>
-              <p className='text-md lg:text-lg text-white font-semibold whitespace-pre-wrap'>We plan to give back for a bigger cause. Voxel Turtle Club will donate a percentage of each NFT minted to a selected organization that mission ties with rescuing sea turtles and other sea life.</p>            
-            </div>
-
-          </div>
-        
+          </FadeInSection>
         </div>
       </div> 
 
@@ -122,55 +118,62 @@ function App() {
           <div class="flex flex-wrap ">
             <div class="grid grid-cols-1 lg:grid-cols-2 w-full px-4">
               <div>
-                <details class="mb-4">
-                  <summary class="py-2 px-4 text-2xl font-semibold cursor-pointer	">
-                    What are NFTs?
-                  </summary>
-                  <p className='px-10 text-lg font-semibold whitespace-pre-wrap'>
-                    Non-Fungible Tokens
-                    <br />
-                    <br />
-                    NFTs are tokens that we can use to represent ownership of unique items. They let us tokenise things like art, collectibles, even real estate. They can only have one official owner at a time and they're secured by the Ethereum blockchain – no one can modify the record of ownership or copy/paste a new NFT into existence.
-                    <br />
-                    <br />
-                    <a href='https://ethereum.org/en/nft/'>Learn more here</a> 
-                  </p>
-                </details>
+                <FadeInSection>
+                  <details class="mb-4">
+                    <summary class="py-2 px-4 text-2xl font-semibold cursor-pointer	">
+                      What are NFTs?
+                    </summary>
+                    <p className='px-10 text-lg font-semibold whitespace-pre-wrap'>
+                      Non-Fungible Tokens
+                      <br />
+                      <br />
+                      NFTs are tokens that we can use to represent ownership of unique items. They let us tokenise things like art, collectibles, even real estate. They can only have one official owner at a time and they're secured by the Ethereum blockchain – no one can modify the record of ownership or copy/paste a new NFT into existence.
+                      <br />
+                      <br />
+                      <a href='https://ethereum.org/en/nft/'>Learn more here</a> 
+                    </p>
+                  </details>  
+                </FadeInSection>
+                <FadeInSection>
+                  <details class="mb-4">
+                    <summary class="py-2 px-4 text-2xl font-semibold cursor-pointer	 ">
+                      How do I get a Voxel Turtle?
+                    </summary>
+                    <p className='px-10 text-lg font-semibold whitespace-pre-wrap'>
+                      Voxel Turtles will be available in early 2022. You will need an Ethereum wallet with Ethereum funds.
+                      <br />
+                      <br />
+                      <a href='https://ethereum.org/en/wallets/'>Learn more here</a> 
+                    </p>
+                  </details>
+                </FadeInSection>
+                <FadeInSection>
+                  <details class="mb-4">
+                    <summary class="py-2 px-4 text-2xl font-semibold cursor-pointer	">
+                      How much will it cost to mint a Voxel Turtle?
+                    </summary>
+                    <p className='px-10 text-lg font-semibold whitespace-pre-wrap'>
+                      Voxel Turtles will start at 0.055 ETH. 
+                      <br />
+                      The 20 1/1 rare Voxel Turtles will start at 0.1 ETH.
+                    </p>
+                  </details>
+                </FadeInSection>
 
-                <details class="mb-4">
-                  <summary class="py-2 px-4 text-2xl font-semibold cursor-pointer	 ">
-                    How do I get a Voxel Turtle?
-                  </summary>
-                  <p className='px-10 text-lg font-semibold whitespace-pre-wrap'>
-                    Voxel Turtles will be available in early 2022. You will need an Ethereum wallet with Ethereum funds.
-                    <br />
-                    <br />
-                    <a href='https://ethereum.org/en/wallets/'>Learn more here</a> 
-                  </p>
-                </details>
-
-                <details class="mb-4">
-                  <summary class="py-2 px-4 text-2xl font-semibold cursor-pointer	">
-                    How much will it cost to mint a Voxel Turtle?
-                  </summary>
-                  <p className='px-10 text-lg font-semibold whitespace-pre-wrap'>
-                    Voxel Turtles will start at 0.055 ETH. 
-                    <br />
-                    The 20 1/1 rare Voxel Turtles will start at 0.1 ETH.
-                  </p>
-                </details>
-
-                <details class="mb-4">
-                  <summary class="py-2 px-4 text-2xl font-semibold cursor-pointer	">
-                    When will Voxel Turtles will be available?
-                  </summary>
-                  <p className='px-10 text-lg font-semibold whitespace-pre-wrap'>
-                    Early 2022
-                  </p>
-                </details>
+                <FadeInSection>
+                  <details class="mb-4">
+                    <summary class="py-2 px-4 text-2xl font-semibold cursor-pointer	">
+                      When will Voxel Turtles will be available?
+                    </summary>
+                    <p className='px-10 text-lg font-semibold whitespace-pre-wrap'>
+                      Early 2022
+                    </p>
+                  </details>
+                </FadeInSection>
               </div>
-
-              <img src={sleep} className='w-1/2 justify-self-center self-start '/>
+              <FadeInSection>
+                <img src={sleep} className='w-1/2 justify-self-center self-start '/>
+              </FadeInSection>
             </div>
           </div>
         </div>
