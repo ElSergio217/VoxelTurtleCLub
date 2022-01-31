134ea1a938c05aa1e2956e421bcb992170b71cc1
sergio garcia <sergioa.garcia217@gmail.com>
2022-01-31 13:42:41 -0500

    updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -66,14 +66,14 @@ function Home() {
         </div>
         <img src={header} className='max-h-32 cursor-pointer' onClick={() =>scrollToPane(0)}/>
         <div class="flex-1 flex justify-center ml-auto self-center">
-          <a href="https://twitter.com/VoxelTurtleClub" target={"_blank"} class="inline-block text-lg font-semibold text-white cursor-pointer self-center m-5" onClick={() =>scrollToPane(4)}><FaTwitter size={32}/></a>
-          {/* <a class="inline-block text-lg font-semibold text-white cursor-pointer self-center m-5" onClick={() =>scrollToPane(4)}><FaInstagram size={32}/></a> */}
+          <a href="https://twitter.com/VoxelTurtleClub" target={"_blank"} class="inline-block text-lg font-semibold text-white cursor-pointer self-center m-5" ><FaTwitter size={32}/></a>
+          <a href="https://www.instagram.com/voxelturtleclub/" target={"_blank"} class="inline-block text-lg font-semibold text-white cursor-pointer self-center m-5"><FaInstagram size={32}/></a>
           <a class="turtleButton inline-block text-lg font-semibold px-6 py-4 text-black cursor-pointer self-center m-5" onClick={() =>scrollToPane(4)}>Coming Soon</a>
         </div>
       </nav>
 
       <div className='flex h-screen turtleHeader'  ref={pageRef[0]}>
-        <div class="grid lg:grid-cols-2 md:grid-cols-1  m-auto container">
+        <div class="grid lg:grid-cols-2 md:grid-cols-1  m-auto w-5/6">
           <img src={logo} className="w-3/4 lg:w-full "/>
           <div className='text-center m-auto w-3/4'>
             <h1 className='text-xl lg:text-2xl text-white font-semibold'>It's those aquatic reptiles. 1,800 Voxel Turtles are coming to the Ethereum network.</h1>
@@ -85,8 +85,8 @@ function Home() {
             </button>
             </a>
             <br/>
-            <a class="inline-block text-lg font-semibold text-white cursor-pointer self-center m-5 lg:invisible visible" onClick={() =>scrollToPane(4)}><FaTwitter size={32}/></a>
-            {/* <a class="inline-block text-lg font-semibold text-white cursor-pointer self-center m-5 md:invisible visible" onClick={() =>scrollToPane(4)}><FaInstagram size={32}/></a> */}
+            <a href="https://twitter.com/VoxelTurtleClub" target={"_blank"} class="inline-block text-lg font-semibold text-white cursor-pointer self-center m-5 lg:hidden visible"><FaTwitter size={32}/></a>
+            <a href="https://www.instagram.com/voxelturtleclub/" target={"_blank"} class="inline-block text-lg font-semibold text-white cursor-pointer self-center m-5 md:invisible visible"><FaInstagram size={32}/></a>
           </div>
         </div>
       </div>   
@@ -96,7 +96,7 @@ function Home() {
 
 
       <div className='flex p-6 turtleHeader' ref={pageRef[1]}>
-          <div class=" m-auto container"> 
+          <div class="m-auto w-5/6"> 
               <div class="grid lg:grid-cols-2 grid-cols-1 m-auto container text-center mt-20 ">
                 <p className='text-xl lg:text-2xl text-white font-semibold whitespace-pre-wrap text-left w-full mx-auto mt-5'>
                   <FadeInSection>
@@ -104,7 +104,7 @@ function Home() {
                   </FadeInSection>
                   <br/>
                   <FadeInSection>
-                    Voxel Turtle Club is a digitally generated NFT collection of turtles with different types of attributes such as head wear, face style, tops and more. Each Voxel Turtle are secured by the Ethereum blockchain with one owner at a time. Our mission is to use the Voxel Turtle Club to build awareness and contribute to turtles and sea life rescue. 
+                    Voxel Turtle Club is a digitally generated NFT collection of turtles with different types of attributes such as head wear, face style, tops and more. Each Voxel Turtle are secured by the Polygon Network with one owner at a time. Our mission is to use the Voxel Turtle Club to build awareness and contribute to turtles and sea life rescue. 
                   </FadeInSection>
                 </p>
                 <img src={turle1} className='w-1/3 justify-self-center self-end '/>
@@ -114,7 +114,7 @@ function Home() {
 
   
       <div className='flex py-28 turtleHeader' ref={pageRef[2]}>
-        <div class=" m-auto container">
+        <div class=" m-auto w-5/6">
           <FadeInSection>
             <h1 className='text-4xl lg:text-6xl	text-white font-semibold mt-5 text-center w-full m-auto '>Voxel Turtle Roadmap</h1>   
           </FadeInSection>
@@ -191,10 +191,10 @@ function Home() {
                       How do I get a Voxel Turtle?
                     </summary>
                     <p className='px-10 text-lg font-semibold whitespace-pre-wrap'>
-                      Voxel Turtles will be available in early 2022. You will need an Ethereum wallet with Ethereum funds.
+                      Voxel Turtles will be available in early 2022. You will need an Polygon wallet with MATIC funds.
                       <br />
                       <br />
-                      <a href='https://ethereum.org/en/wallets/'>Learn more here</a> 
+                      <a href='https://wallet.polygon.technology/'>Learn more here</a> 
                     </p>
                   </details>
                 </FadeInSection>
@@ -204,9 +204,9 @@ function Home() {
                       How much will it cost to mint a Voxel Turtle?
                     </summary>
                     <p className='px-10 text-lg font-semibold whitespace-pre-wrap'>
-                      Voxel Turtles will start at 0.055 ETH. 
+                      Voxel Turtles will start at 80 MATIC. 
                       <br />
-                      The 20 1/1 rare Voxel Turtles will start at 0.1 ETH.
+                      The 20 1/1 rare Voxel Turtles will start at 120 MATIC.
                     </p>
                   </details>
                 </FadeInSection>
@@ -217,7 +217,7 @@ function Home() {
                       When will Voxel Turtles will be available?
                     </summary>
                     <p className='px-10 text-lg font-semibold whitespace-pre-wrap'>
-                      Early 2022
+                      February 15th, 2022
                     </p>
                   </details>
                 </FadeInSection>
